c022adbaa04b5cae2dbea5c17a22051b3713b05d
YOUR FULL NAMEAndres <a_espinosa@live.com>
2016-10-13 10:06:07 -0400

    Created models Seeds before migrate

--- got/db/seeds.rb
+++ got/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Houses
+houses = House.create([{ tittle: 'Big House' }])
+houses = House.create([{ tittle: 'Small House' }])
+houses = House.create([{ tittle: 'Bigger House' }])
+houses = House.create([{ tittle: 'Smaller House' }])
+houses = House.create([{ tittle: 'Humongous House' }])
+houses = House.create([{ tittle: 'Tiny House' }])
+
+#Characters
+characters = Character.create([{ Name: 'Jon Snow' }])
+characters = Character.create([{ Name: 'Arya Stark' }])
+characters = Character.create([{ Name: 'Ramsay Bolton' }])
+characters = Character.create([{ Name: 'Gregor Clegane' }])
+characters = Character.create([{ Name: 'Tyrion Lannister' }])
+characters = Character.create([{ Name: 'Theon Greyjoy' }])
