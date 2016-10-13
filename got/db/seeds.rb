c189b6237e938ff125dde2b99d1a13e6b830840b
YOUR FULL NAMEAndres <a_espinosa@live.com>
2016-10-13 14:25:24 -0400

    Blank canvas again

--- got/db/seeds.rb
+++ got/db/seeds.rb
@@ -5,19 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-#Houses
-houses = House.create([{ name: 'Big House' }])
-houses = House.create([{ name: 'Small House' }])
-houses = House.create([{ name: 'Bigger House' }])
-houses = House.create([{ name: 'Smaller House' }])
-houses = House.create([{ name: 'Humongous House' }])
-houses = House.create([{ name: 'Tiny House' }])
-
-#Characters
-characters = Character.create([{ name: 'Jon Snow' }])
-characters = Character.create([{ name: 'Arya Stark' }])
-characters = Character.create([{ name: 'Ramsay Bolton' }])
-characters = Character.create([{ name: 'Gregor Clegane' }])
-characters = Character.create([{ name: 'Tyrion Lannister' }])
-characters = Character.create([{ name: 'Theon Greyjoy' }])
